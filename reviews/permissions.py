700db3dc4ba28bf25d464433bbde6439e11fb63c
girik108 <irek@docker-ubuntu2004.ru-central1.internal>
2021-02-10 05:17:47 +0000

    Return migrations

--- reviews/permissions.py
+++ reviews/permissions.py
@@ -7,7 +7,9 @@ class CustomerAccessPermission(BasePermission):
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return any([request.method in SAFE_METHODS,
-                    obj.author == request.user,
-                    request.user.is_authenticated and (
-                    request.user.is_moderator or request.user.is_admin),])
+        return any([
+            request.method in SAFE_METHODS,
+            obj.author == request.user,
+            request.user.is_authenticated and (
+                    request.user.is_moderator or request.user.is_admin),
+        ])
